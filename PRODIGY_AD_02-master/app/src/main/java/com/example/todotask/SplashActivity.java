a46fb7e145f73b14312ee6da6fb5015f611fdedf
sanjana-321 <sanjanasharma1308@gmail.com>
2024-07-25 15:51:20 +0530

    This is my commit

---
+++ PRODIGY_AD_02-master/app/src/main/java/com/example/todotask/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.todotask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Check if getSupportActionBar() returns non-null before calling hide()
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        final Intent i = new Intent(SplashActivity.this, MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+    }
+
+}
